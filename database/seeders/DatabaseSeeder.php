3d437a6a55b8324ef82e51dc10ea5e55c74e62e7
Adibayu Luthfiansyah Setyawan <106433930+Adibayuluthfiansyah@users.noreply.github.com>
2025-10-23 00:08:49 +0700

    add VenueownerSeeder to database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
-use Database\Seeders\UserSeeder;
 use Database\Seeders\VenueSeeder;
+use Database\Seeders\VenueOwnerSeeder;
 
 
 class DatabaseSeeder extends Seeder
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
-            VenueSeeder::class,
+            // UserSeeder::class,
+            VenueSeeder::class,       // 1. Buat Venues, Fields, dan TimeSlots
+            VenueOwnerSeeder::class,
         ]);
     }
 }
